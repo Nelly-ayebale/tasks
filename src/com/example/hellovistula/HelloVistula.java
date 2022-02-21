283aed4d5f71e1ac622090764b8ac993ef3d6cef
Nelly-ayebale <ayebalenelly26@gmail.com>
2022-02-21 17:00:52 +0100

    Added "this is my first project"

--- src/com/example/hellovistula/HelloVistula.java
+++ src/com/example/hellovistula/HelloVistula.java
@@ -2,6 +2,7 @@ package com.example.hellovistula;
 
 public class HelloVistula {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula");
+        System.out.println("Hello Vistula,this is my first project");
+
     }
 }
